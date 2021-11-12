0a1e4ff80c304cd79bfeaabfe170f4e80971dff4
deepakmench18 <deepakmench18@gmail.com>
2021-11-12 20:32:20 +0530

    Program

---
+++ String Replace.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+
+Created on Mon Nov  8 12:28:33 2021
+@author: Deepak Mench
+
+"""
+username = input("Enter String");
+if len(username) > 3 :
+    print("Hello  " + username + "  How are you ..?" );
+    

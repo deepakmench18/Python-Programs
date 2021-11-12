7032810eaab50bb8e4642b413a13f09c704d4d68
deepakmench18 <deepakmench18@gmail.com>
2021-11-12 21:29:40 +0530

    program

---
+++ StopWatch.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Nov 12 12:29:19 2021
+
+@author: Deepak Mench
+@Title : Write a Stopwatch Program for measuring the time that elapses between
+the start and end clicks
+"""
+
+import time
+start = time.time()
+print("hello")
+end = time.time()
+print(end - start)
